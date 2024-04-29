582f7d8bb59c4fc0716a3a55d8b87d86750db0b2
sparkleHazard <sparklehazard@protonmail.com>
2024-04-29 10:15:32 +0100

    Adds OnionBoy layout to Reviung34

---
+++ keyboards/reviung/reviung34/keymaps/onionboy/config.h
@@ -0,0 +1,28 @@
+/* Copyright 2015-2021 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#undef WS2812_DI_PIN
+#define WS2812_DI_PIN 23
+#undef RGBLED_NUM
+#define RGBLED_NUM 1
+
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+#define ENCODER_RESOLUTION 4
